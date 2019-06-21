88222b81c039e11e35b11cee6180896cc993cab2
Unnit Metaliya <unnitjmetaliya@gmail.com>
2019-06-21 10:31:28 -0400

    v 1.0

---
+++ brief.js
@@ -0,0 +1,22 @@
+let i = {};
+i.am = "a web developer";
+i.know = [
+  "html",
+  "css",
+  "javascript",
+  "react",
+  "angular",
+  "nodejs",
+  "PHP",
+  "python",
+  "SQL",
+  "MongoDB",
+  "PostgresSQL",
+  "GraphQL"
+];
+i.have = [
+  "2 years of experience",
+  "a Canadian Master's degree in Computer Science"
+];
+i.helped = "Blackberry to create high quality web applications";
+i.love = ["loooong road trips", "hip hop music", "to teach people code"];
